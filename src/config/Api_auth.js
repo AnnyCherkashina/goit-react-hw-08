e200ed5c6df0eb19ee3d3798214b027235ac0f99
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2025-05-09 22:26:49 +0300

    update3

--- src/config/Api_auth.js
+++ src/config/Api_auth.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const authApi = axios.create({
     baseURL: `https://connections-api.herokuapp.com/`,
 });
-
 export const setToken = (token) => {
     authApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
