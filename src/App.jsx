f978f13764b86943a7aff1f10703f847311e9961
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2025-05-09 21:39:04 +0300

    changing folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,61 @@
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import "./App.css";
-import ContactForm from "./components/ContactForm/ContactForm";
-import ContactList from "./components/ContactList/ContactList";
-import SearchBox from "./components/SearchBox/SearchBox";
-import { fetchContacts } from "./redux/contactsOps";
 import { useDispatch, useSelector } from "react-redux";
-import { selectIsLoading } from "./redux/contactsSlice";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { refreshThunk } from "./redux/auth/operations";
+import PrivateRoute from "./routes/PrivateRoute/PrivateRoute";
+import PublicRoutes from "./routes/PublicRoutes/PublicRoutes";
+import { selectIsRefresh } from "./redux/auth/slice";
+import Loader from "./components/Loader/Loader";
 
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const Contacts = lazy(() => import("./pages/Contacts/Contacts"));
+const Login = lazy(() => import("./pages/Login/Login"));
+const Register = lazy(() => import("./pages/Register/Register"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 function App() {
-  const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
+
+  const isRefreshing = useSelector(selectIsRefresh);
+
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshThunk());
   }, [dispatch]);
-  return (
-    <div className="wrapper">
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      {isLoading && <h2>Loading...</h2>}
-      <ContactList />
-    </div>
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
+    <>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute>
+                  <Contacts />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="register" element={<Register />} />
+          <Route
+            path="login"
+            element={
+              <PublicRoutes>
+                <Login />
+              </PublicRoutes>
+            }
+          />
+        </Routes>
+        <ToastContainer theme="colored" />
+      </Suspense>
+    </>
   );
 }
 
