3acb1a95136283ca7e05e737fd7ad4d25ea75510
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2025-05-09 22:43:18 +0300

    update 4

--- src/config/Api_auth.js
+++ src/config/Api_auth.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 export const authApi = axios.create({
-    baseURL: `https://connections-api.herokuapp.com/`,
+    baseURL: `https://connections-api.goit.global/`,
 });
+
 export const setToken = (token) => {
     authApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 export const clearToken = () => {
-    authApi.defaults.headers.common.Authorization = ` `;
+    authApi.defaults.headers.common.Authorization = ``;
 };
